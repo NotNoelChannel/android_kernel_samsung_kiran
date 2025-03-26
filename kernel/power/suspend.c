943cabb05f33e226bc765abcb8493736e124b50e
NotNoelChannel <52098241+NotNoelChannel@users.noreply.github.com>
2025-03-26 16:53:25 +0100

    fix issue: kernel/power/suspend.c

--- kernel/power/suspend.c
+++ kernel/power/suspend.c
@@ -329,11 +329,6 @@ static int enter_state(suspend_state_t state)
 	if (suspend_test(TEST_FREEZER))
 		goto Finish;
 
-	pr_debug("PM: Entering %s sleep\n", pm_states[state].label);
-	pm_restrict_gfp_mask();
-	error = suspend_devices_and_enter(state);
-	pm_restore_gfp_mask();
-
  Finish:
 	pr_debug("PM: Finishing wakeup.\n");
 	suspend_finish();
